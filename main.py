560d4c3db359a339dd19e556da8371e383e43dcc
CopperStock <jgbeyea@gmail.com>
2025-06-03 22:53:02 -0500

    D: main & stats

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+def main():
+	book_path = "books/frankenstein.txt"
+	text = get_book_text(book_path)
+	count = from stats import word_count(text)
+	print(f"{count} words found in the document")
+
+def get_book_text(path):
+	with open(path) as f:
+		return f.read()
+
+
+	 
+
+main()
---
+++ stats.py
@@ -0,0 +1,6 @@
+
+def word_count(text):
+	words = text.split()
+	count = len(words)
+	return count
+	
